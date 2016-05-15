df34d79fc5cef8e2836b1049937d779be4810035
Matthew <matthew.thompson.a@gmail.com>
2016-05-15 18:04:06 -0400

    initial push of all slick testing files and photos

---
+++ slicktest.js
@@ -0,0 +1,15 @@
+$(".slides").slick({
+  fade:true,
+  autoplay:true,
+  autoplaySpeed:1500,
+  arrows: false,
+  dots:true
+});
+
+$(".scroll").slick({
+  arrows:true,
+  dots:true,
+  infinite:true,
+  slidesToShow:3,
+  slidesToScroll:3,
+});
